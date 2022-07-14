43e9d424862234e68081ae850ae0bb268904b323
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2022-07-14 08:14:39 +0700

    change map layout

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="map">
     <div></div>
-    <div class="map-container" ref="gal">
+    <!-- <div class="map-container" ref="gal">
       <iframe 
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3988.91407574767!2d116.8586017!3d-1.2198663!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2df1471b921fb1c3%3A0xfe3fb0adc46c07a9!2sMasjid%20Al%20Auliya%20Balikpapan!5e0!3m2!1sid!2sid!4v1657374874979!5m2!1sid!2sid" 
         width="100%" 
@@ -11,7 +11,7 @@
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade">
       </iframe>
-    </div>
+    </div> -->
   </div>
 </template>
 
