334535bcd0b2e35b95e54b088a7ae00229ba2f6b
Muhammad Adib Arinanda <arinanda.adib@gmail.com>
2022-08-03 16:31:22 +0700

    update config

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
-import VueFab from 'vue-float-action-button'
 import Vue from 'vue'
 import VScrollLock from 'v-scroll-lock'
 import App from './App.vue'
 import Families from './Families.vue'
+import FamiliesAZ from './FamiliesAZ.vue'
 import Friends from './Friends.vue'
 import Divider from './components/Divider.vue'
 import Button from './components/Button.vue'
 import firebase from 'firebase/app'
+import 'firebase/firestore';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
 import VueClipboard from 'vue-clipboard2'
 import VueRouter from 'vue-router'
+import VueHead from 'vue-head'
 
 Vue.config.productionTip = false
 
-Vue.use(VueFab)
+Vue.use(VueHead)
 Vue.use(VScrollLock)
 Vue.use(VueClipboard)
 Vue.use(VueRouter)
@@ -22,8 +24,9 @@ Vue.component('Divider', Divider)
 Vue.component('Button', Button)
 
 const routes = [
-  { path: "/families", component: Families },
+  { path: "/families/za", component: Families },
   { path: "/friends", component: Friends },
+  { path: "/families/az", component: FamiliesAZ },
 ];
 
 const router = new VueRouter({
@@ -31,17 +34,18 @@ const router = new VueRouter({
   mode: 'history'
 });
 
-// use your firebase info
 var firebaseConfig = {
-  apiKey: 'Use your api key',
-  authDomain: 'carepass-test.firebaseapp.com',
-  databaseURL: 'https://carepass-test.firebaseio.com',
-  projectId: 'carepass-test',
-  storageBucket: 'carepass-test.appspot.com',
-  messagingSenderId: '{Use your messagingSenderId}',
-  appId: '{Use your appId}',
+  apiKey: "AIzaSyAtyKOVCGuAQtxJILKYp253JbKJptxzNBA",
+  authDomain: "zulfadib-6be45.firebaseapp.com",
+  databaseURL: "https://zulfadib-6be45-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "zulfadib-6be45",
+  storageBucket: "zulfadib-6be45.appspot.com",
+  messagingSenderId: "980713302102",
+  appId: "1:980713302102:web:b857e47a1ca90a777173f7",
+  measurementId: "G-VCE6X3GBCY"
 }
-firebase.initializeApp(firebaseConfig)
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const db = firebaseApp.firestore();
 
 new AOS.init()
 
